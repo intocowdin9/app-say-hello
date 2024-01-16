fd1c10964261725a3de60c5239e55130a6e5e06e
return <intocowdin9@gmail.com>
2024-01-16 19:59:28 +0700

    go-app-say-hello

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/intocowdin9/app-say-hello
+
+go 1.21.5
+
+require github.com/intocowdin9/go-say-hello/v2 v2.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	go_say_hello "kelas-golang-pzn/go-say-hello"
+)
+
+/* materi go modules */
+func main() {
+	fmt.Println(go_say_hello.SayHello(" rafli"))
+}
